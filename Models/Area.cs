ed7949acfa495c01f9fbeca65554931da1ac1499
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-08-09 12:43:49 +0300

    I did a migration and the tables are visible, and now I am receiving the data.

--- Models/Area.cs
+++ Models/Area.cs
@@ -1,6 +1,22 @@
-﻿namespace ShelterApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ShelterApi.Models
 {
     public class Area
     {
+        public int Id { get; set;}
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string Neighborhood { get; set; } = string.Empty;
+        [Required]
+        [StringLength(20)]
+        public string AreaCode { get; set; } = string.Empty;
+        public int RiskLevel { get; set; }
+
+        public ICollection<Shelter> Shelters { get; set; } = new List<Shelter>(); 
     }
 }
